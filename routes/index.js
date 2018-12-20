f250ff8743e7f551617df8d1f56dd744ff3b90b5
Fatih Ridha <abgfatih@gmail.com>
2018-12-19 18:54:15 -0800

    throw err instead of redirect

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,16 @@
 module.exports = {
-    getHomePage: (req, res) => {
-        let query = "SELECT * FROM `player` ORDER BY `rating` DESC"; // query database to get all the players
+  getHomePage: (req, res) => {
+    let query = 'SELECT * FROM `player` ORDER BY `rating` DESC'; // query database to get all the players
 
-        // execute query
-        db.query(query, (err, result) => {
-            if (err) {
-                res.redirect('/');
-            }
-            res.render('index.ejs', {
-                title: "Nuzlocke Ratings | Leaderboard",
-                players: result
-            });
-        });
-    },
+    // execute query
+    db.query(query, (err, result) => {
+      if (err) {
+        throw err;
+      }
+      res.render('index.ejs', {
+        title: 'Nuzlocke Ratings | Leaderboard',
+        players: result
+      });
+    });
+  }
 };
