cdc50defe6a34b881c3f4294715809a810116e79
Fatih Ridha <abgfatih@gmail.com>
2019-01-13 12:41:57 -0800

    fix form encoding

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ const app = require('./custom-express');
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 // session for maintaining logins
 app.use(
